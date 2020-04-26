9a1b0d20ae90702bc8daafef715054755bf6fbd1
Michael Silva <msilva@aggienetwork.com>
2020-04-26 14:48:08 -0500

    removed duplicate JS file

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,15 @@
-var cityInputEl = document.getElementById("city-form");
+var cityInputEl = document.getElementById("city-form"); 
 
-var inputSubmitHandler = function(event) {
+var myKey = "7aeefed14e670086fed17e50b13e3bf5"
+
+var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + cityInputEl + "&units=imperial&appid=" + myKey;
+
+var getCurrentWeather = function() {
     event.preventDefault();
-    var cityName = cityInputEl.value.trim();
-};
-
-var getCurrentWeather = function(city) {
-    var apiUrl = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=7aeefed14e670086fed17e50b13e3bf5"
-
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            console.log(data);
-        });
+    fetch(apiUrl)
+    .then(function(response) {
+        return response.json();
     });
 };
-cityInputEl.addEventListener("submit", inputSubmitHandler);
+
+cityInputEl.addEventListener("submit", getCurrentWeather);
